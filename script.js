d0c129e98fb348db4f3a2f55345399ab8f0bf148
Kievsky332 <falileevry@gmail.com>
2026-02-22 19:03:26 +0300

    Комментарии

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const text = document.getElementById("chat");
 const unshow  = document.getElementById("centered");
 let a = document.getElementById("inpt");
 const mess = document.getElementById("message");
-bd =[]
+bd =[] //база данных 
 function chat(){
     
     if (a.value !==""){
@@ -14,7 +14,7 @@ function chat(){
         warn.style.display = "inherit";
     };
 };
-function add_message(){
+function add_message(){ //Вставляем в бд и выводим
     const text = document.getElementById("text");
     bd += "<p>"+a.value +": "+text.value+ "</p>";
     mess.innerHTML = bd;
